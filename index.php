143c4443a7ecec29776bd87716fb0be111b040b4
seejansunar <czansunar0098@gmail.com>
2022-06-05 15:24:09 +0545

    created simple api

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+    $url = "http://localhost/api-development-using-php/server/";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    $result = json_decode($result, true);
+
+    if(isset($result['status']))
+    {
+        if($result['status'] == true)
+        {
+            if(isset($result['result']))
+            {
+                if($result['result'] == true)
+                {
+                    echo '<pre>';
+                    print_r($result['data']);
+                }else{
+                    echo '<pre>';
+                    print_r($result['data']);
+                }
+            }
+        }
+    }
+
+?>
